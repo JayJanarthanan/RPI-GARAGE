20e92e082c274672406e4255eece77b09c6a959f
Jay Janarthanan <jay-j@live.com>
2017-04-14 09:34:42 +1200

    initial files

--- door.py
+++ door.py
@@ -1,22 +1,19 @@
- #!/usr/bin/python   
- # Import required Python libraries   
- import RPi.GPIO as GPIO   
- import time   
- # Use BM GPIO references instead of physical pin numbers   
- GPIO.setmode(GPIO.BOARD)   
- # init list with pin numbers   
- pinList = [2]   
- # loop through pins and set mode and state to 'low'   
- for i in pinList:   
-      GPIO.setup(11, GPIO.OUT)   
-      GPIO.output(11, GPIO.HIGH)   
- def trigger():  
-       for i in pinList:  
-            GPIO.output(11, GPIO.LOW)  
-            time.sleep(0.5)  
-            GPIO.output(11, GPIO.HIGH)  
-            GPIO.cleanup()   
- try:   
-      trigger()   
- except KeyboardInterrupt:   
-      print " Quit" # Reset GPIO settings  
+#!/usr/bin/python   
+# Import required Python libraries   
+import RPi.GPIO as GPIO   
+import time   
+# Use BM GPIO references instead of physical pin numbers   
+GPIO.setmode(GPIO.BOARD)   
+
+GaragePin = 11
+GPIO.setup(GaragePin, GPIO.OUT)   
+GPIO.output(GaragePin, GPIO.HIGH)   
+def trigger():  
+	GPIO.output(GaragePin, GPIO.LOW)  
+	time.sleep(0.5)  
+	GPIO.output(GaragePin, GPIO.HIGH)  
+	GPIO.cleanup()   
+try:   
+  trigger()   
+except KeyboardInterrupt:   
+  print " Quit" # Reset GPIO settings  
