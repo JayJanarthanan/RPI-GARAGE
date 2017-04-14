a2552384c94b38b8354d4318044a3ab5d57ceaa0
Jay Janarthanan <jay-j@live.com>
2017-04-14 18:15:54 +1200

    Uh oh
    I have no fking idea what I'm doing. Please help Alyssa.

---
+++ garage-node/app.js
@@ -0,0 +1,61 @@
+var express = require('express'),
+	path = require('path'),
+	config = require('./config'),
+	async = require('async'),
+	gpio = require('pi-gpio'),
+	usonic = require('r-pi-usonic'),
+	app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use('/', express.static(__dirname + '/public'));
+
+usonic.init(function (error) {
+    if (error) {
+
+    } else {
+       
+    }
+});
+
+
+var sensor = usonic.createSensor(24, 23, 450);
+var distance = sensor();
+
+function delayPinWrite(pin, value, callback) {
+	setTimeout(function() {
+		gpio.write(pin, value, callback);
+	}, config.RELAY_TIMEOUT);
+}
+
+app.get("/api/ping", function(req, res) {
+	res.json("pong");
+});
+
+
+app.post("/api/garage/right", function(req, res) {
+	async.series([
+		function(callback) {
+			// Open pin for output
+			gpio.open(config.RIGHT_GARAGE_PIN, "output", callback);
+		},
+		function(callback) {
+			// Turn the relay on
+			gpio.write(config.RIGHT_GARAGE_PIN, config.RELAY_ON, callback);
+		},
+		function(callback) {
+			// Turn the relay off after delay to simulate button press
+			delayPinWrite(config.RIGHT_GARAGE_PIN, config.RELAY_OFF, callback);
+		},
+		function(err, results) {
+			setTimeout(function() {
+				// Close pin from further writing
+				gpio.close(config.RIGHT_GARAGE_PIN);
+				// Return json
+				res.json("ok");
+			}, config.RELAY_TIMEOUT);
+		}
+	]);
+});
+
+app.listen(app.get('port'));
---
+++ garage-node/config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.RIGHT_GARAGE_PIN = 14;
+
+config.RELAY_ON = 0;
+config.RELAY_OFF = 1;
+config.RELAY_TIMEOUT = 500;
+
+module.exports = config;
