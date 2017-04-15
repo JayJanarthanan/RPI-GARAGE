21ed32df4ba42928ea9f7128d108c620373f14bf
Jay Janarthanan <jay-j@live.com>
2017-04-15 17:11:48 +1200

    camera working

--- garage-node/app.js
+++ garage-node/app.js
@@ -5,24 +5,81 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var async = require('async');
 var gpio = require('rpi-gpio');
-var port = process.env.PORT || 3000;
+var fs = require('fs');
 
+var spawn = require('child_process').spawn;
+var proc;
+
+var port = process.env.PORT || 3000;
 var pin = 11;
 
 
 //Setup the web page and processes
 
-app.get('/', function(req, res){
-    res.sendfile(__dirname + '/index.html');
-});
-
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
-
 // HTML static file goes in the /public folder
-//app.set('port', port);
-//app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
+
+
+var sockets = {};
+//Setup the Camera connection
+io.on('connection', function(socket) {
+ 
+  sockets[socket.id] = socket;
+  console.log("Total clients connected : ", Object.keys(sockets).length);
+ 
+  socket.on('disconnect', function() {
+    delete sockets[socket.id];
+ 
+    // no more sockets, kill the stream
+    if (Object.keys(sockets).length == 0) {
+      app.set('watchingFile', false);
+      if (proc) proc.kill();
+      fs.unwatchFile('./public/cam.jpg');
+    }
+  });
+ 
+  socket.on('start-stream', function() {
+    startStreaming(io);
+  });
+ 
+});
+
+
+function stopStreaming() {
+  if (Object.keys(sockets).length == 0) {
+    app.set('watchingFile', false);
+    if (proc) proc.kill();
+    fs.unwatchFile('./public/cam.jpg');
+  }
+}
+ 
+function startStreaming(io) {
+ 
+  if (app.get('watchingFile')) {
+    io.sockets.emit('liveStream', 'cam.jpg');
+    return;
+  }
+ 
+  var args = ["-vf", "-hf", "-w", "640", "-h", "480", "-o", "./public/cam.jpg", "-t", "999999999", "-tl", "100"];
+  proc = spawn('raspistill', args);
+ 
+  console.log('Watching for changes...');
+ 
+  app.set('watchingFile', true);
+ 
+  fs.watchFile('./public/cam.jpg', function(current, previous) {
+    io.sockets.emit('liveStream', '/cam.jpg');
+  })
+ 
+}
+
+
+
+
+
 
 
 
@@ -106,7 +163,7 @@ var getStatus = function () {
      else {
 		status = "Unknown";
     }
-	process.stdout.write(status + " " + distance);
+	//process.stdout.write(status + " " + distance);
     return [status, distance];
   };
 
